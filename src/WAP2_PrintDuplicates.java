104f7e0c1e15341f768fed73ce56e627e121eb22
shreyas-32 <challashreyas5@gmail.com>
2023-01-02 13:47:35 +0530

    My commit

---
+++ src/WAP2_PrintDuplicates.java
@@ -0,0 +1,25 @@
+public class WAP2_PrintDuplicates
+{
+    public static void main(String[] args)
+    {
+        String s = "HardikPandya ";
+        char ch[] = s.toCharArray();
+        int count;
+        for(int i=0;i<ch.length;i++) {
+            count = 1;
+            for(int j=i+1;j<ch.length;j++) {
+                if(ch[i] == ch[j] && ch[i] != ' ' ) {
+                    count++;
+                    ch[j] = 0;
+                }
+            }
+            if(count>1) {
+                System.out.println(ch[i]);
+            }
+        }
+
+    }
+
+}
+
+
